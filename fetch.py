b971bc0d463eafd47a99a9cdd41baad7d35ae9f4
cjfox493004 <cjfox493@gmail.com>
2026-01-21 15:49:49 +0000

    did some practice not working

---
+++ fetch.py
@@ -0,0 +1,36 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('playoffs.db')
+cursor = conn.cursor()
+
+query = """
+    SELECT *
+    FROM teams
+"""
+cursor.execute(query)
+records = cursor.fetchall()
+conn.close()
+
+#Printing all the records
+#print(records)
+
+#printing all the records in a list
+#records_df = pd.DataFrame(records,columns = ["id", "City", "Name"])
+#print(records_df)
+
+#cities = []
+#for record in records:
+  #cities.append(record[1])
+#print(cities)
+
+
+#length_cities = []
+#for record in records:
+   #length_cities.append(len(record[1]))
+#print(length_cities)
+
+length_team = []
+for record in records:
+   length_team.append(len(record([1, 2])))
+print(length_team)
